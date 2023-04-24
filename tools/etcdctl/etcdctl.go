f9333e63f148fcafc09cca74371a99c6d39e14b0
kenor <1154716550@qq.com>
2023-04-24 08:34:30 -0700

    add etcd start and dbug

--- tools/etcdctl/etcdctl.go
+++ tools/etcdctl/etcdctl.go
@@ -19,12 +19,9 @@ func Put(cli *clientv3.Client, k string, v string) error {
 		return errors.New("client is null")
 	}
 
-	ctx, err1 := context.WithTimeout(context.Background(), 5*time.Second)
-	if err1 != nil {
-		return errors.New("context open err")
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	_, err := cli.Put(ctx, k, v)
-
+	cancel()
 	if err != nil {
 		return errors.New("etcd put error")
 	}
@@ -36,12 +33,10 @@ func Get(client *clientv3.Client, k string) (*clientv3.GetResponse, error) {
 		return nil, errors.New("client is null")
 	}
 
-	ctx, err1 := context.WithTimeout(context.Background(), 5*time.Second)
-	if err1 != nil {
-		return nil, errors.New("context open err")
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	
 	ret, err := client.Get(ctx, k)
-
+	cancel()
 	if err != nil {
 		return nil, errors.New("etcd get error")
 	}
@@ -53,12 +48,9 @@ func GetWithPrefix(client *clientv3.Client, k string) (*clientv3.GetResponse, er
 		return nil, errors.New("client is null")
 	}
 
-	ctx, err1 := context.WithTimeout(context.Background(), 5*time.Second)
-	if err1 != nil {
-		return nil, errors.New("context open err")
-	}
+	ctx, cancel:= context.WithTimeout(context.Background(), 5*time.Second)
 	ret, err := client.Get(ctx, k, clientv3.WithPrefix())
-
+	cancel()
 	if err != nil {
 		return nil, errors.New("etcd get error")
 	}
@@ -70,12 +62,10 @@ func Delete(client *clientv3.Client, k string) error {
 		return errors.New("client is null")
 	}
 
-	ctx, err := context.WithTimeout(context.Background(), 5*time.Second)
-	if err != nil {
-		return errors.New("context open err")
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
 	_, err1 := client.Delete(ctx, k)
+	cancel()
 
 	if err1 != nil {
 		return errors.New("etcd delete error")
@@ -92,18 +82,18 @@ func Watch(client *clientv3.Client, k string) (clientv3.WatchChan, error) {
 }
 
 func Start(dirPath string) (*clientv3.Client, error) {
-	cmd := exec.Command("./etcd")
+	cmd := exec.Command("./etcd_start.sh")
 	cmd.Dir = dirPath
-	err := cmd.Start()
+	err := cmd.Run()
 	if err != nil {
-		fmt.Println("etcd already running")
+		fmt.Println(err)
 	} else {
 		fmt.Println("started etcd")
 	}
 
 	cli, err := clientv3.New(
 		clientv3.Config{
-			Endpoints:   []string{"localhost:2379"},
+			Endpoints:   []string{"127.0.0.1:2379"},
 			DialTimeout: 5 * time.Second,
 		})
 
--- tools/etcdctl/etcdctl_test.go
+++ tools/etcdctl/etcdctl_test.go
@@ -7,7 +7,6 @@ import (
 	"time"
 )
 
-
 // 测试时候，先把本机上的etcd运行起来
 func TestGet(t *testing.T) {
 	cli, err := clientv3.New(
@@ -27,7 +26,7 @@ func TestGet(t *testing.T) {
 func TestPut(t *testing.T) {
 	cli, err := clientv3.New(
 		clientv3.Config{
-			Endpoints:   []string{"localhost:2379"},
+			Endpoints:   []string{"127.0.0.1:2379"},
 			DialTimeout: 5 * time.Second,
 		})
 	if err != nil {
@@ -37,4 +36,27 @@ func TestPut(t *testing.T) {
 	defer cli.Close()
 	err = Put(cli, "key", "val")
 	fmt.Println(err)
+}
+
+func TestStart(t *testing.T) {
+	start, err := Start("/home/os/minik8s/minik8s/tools/etcdctl")
+	if err != nil {
+		fmt.Println("start error")
+		return
+	}
+
+	Put(start, "k1", "v1")
+	get, err := Get(start, "k1")
+	if err != nil {
+
+		fmt.Println("get error")
+	}
+
+	fmt.Println(get)
+	defer func(start *clientv3.Client) {
+		err := start.Close()
+		if err != nil {
+
+		}
+	}(start)
 }
