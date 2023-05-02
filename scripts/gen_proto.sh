d345200a89e2297bda3449303b1bbf046621f8ae
zhaoxi <fly0307@sjtu.edu.cn>
2023-05-02 01:34:05 -0700

    refactor:修改gen_proto.sh脚本

--- scripts/gen_proto.sh
+++ scripts/gen_proto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-proto_gen_path="pkg/proto"
+proto_gen_path="pkg"
 proto_path="./proto"
 
 mkdir -p ./$proto_gen_path
 
-protoc --proto_path=$proto_path --go_out=$proto_gen_path --go_opt=paths=source_relative \
+protoc --go_out=$proto_gen_path --go_opt=paths=source_relative \
   --go-grpc_out=$proto_gen_path --go-grpc_opt=paths=source_relative $proto_path/*
 
 if [ $? -eq 0 ]
