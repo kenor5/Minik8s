d44fad2eac03f79e6baa668f17cfd29beebd1518
ZhaoXi <fly0307@sjtu.edu.cn>
2023-05-09 20:50:08 -0700

    通过基础功能测试，增加pod4_zx.yaml

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -30,7 +30,7 @@ type Kubelet struct {
 	containerManager *ContainerManager.ContainerManager
 }
 
-var kubelet Kubelet
+var kubelet *Kubelet
 
 // newKubelet creates a new Kubelet object.
 func newKubelet() *Kubelet {
@@ -43,7 +43,9 @@ func newKubelet() *Kubelet {
 }
 
 func KubeletObject() *Kubelet {
-	kubelet := newKubelet()
+	if kubelet == nil {
+		kubelet = newKubelet()
+	}
 	return kubelet
 }
 
@@ -69,21 +71,21 @@ func (kl *Kubelet) DeletePod(pod *entity.Pod) error {
 
 	// 实际停止并删除Pod中的所有容器
 	podfunc.DeletePod(containerIds)
-	kl.podManger.DeletePod(pod)
+	//kl.podManger.DeletePod(pod)
 	// 更新Pod的状态
 	pod.Status.Phase = entity.Succeed
 	client.UpdatePodStatus(kubelet.connToApiServer, pod)
 	return nil
 }
 
-func (kl *Kubelet) GetPods() ([]*entity.Pod, error) {
-	pm := kl.podManger.GetPods()
-	return pm, nil
-}
+//func (kl *Kubelet) GetPods() ([]*entity.Pod, error) {
+//	//pm := kl.podManger.GetPods()
+//	//return pm, nil
+//}
 
 func (kl *Kubelet) AddPod(pod *entity.Pod) error {
 	//更新元数据
-	kl.podManger.AddPod(pod)
+	//kl.podManger.AddPod(pod)
 	pod.Status.Phase = entity.Running
 
 	//启动沙箱容器和pod.spec.containers中的容器
@@ -95,10 +97,10 @@ func (kl *Kubelet) AddPod(pod *entity.Pod) error {
 	return nil
 }
 
-func (kl *Kubelet) GetPodByName(namespace string, name string) (*entity.Pod, bool) {
-	pm, ok := kl.podManger.GetPodByName(namespace, name)
-	return pm, ok
-}
+//func (kl *Kubelet) GetPodByName(namespace string, name string) (*entity.Pod, bool) {
+//	pm, ok := kl.podManger.GetPodByName(namespace, name)
+//	return pm, ok
+//}
 
 func (kl *Kubelet) RegisterNode() error {
 	registerNodeRequest := &pb.RegisterNodeRequest{
